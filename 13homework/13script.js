00668acf386e1f0f323160f974b5e50df4e69e12
sandrodavituri95-code <sandrodavituri95@gmail.com>
2026-09-06 22:39:58 +0400

    Reapply "uploading script.js and html files"
    This reverts commit 14bb38d1d2d4a247c5a523020fca9afdb9f83073.

---
+++ 13homework/13script.js
@@ -0,0 +1,11 @@
+const Temperature = prompt("Please enter a temperature in celsius:");
+const celsius = Number(Temperature);
+
+let fahrenheit = (celsius * 9/5) + 32;
+alert(celsius + ("°C is " + fahrenheit + "°F"));
+
+document.getElementById("firstparagraphs").innerHTML = "changed text";
+
+const newParagraph= document .createElement("p")
+newParagraph. textContent = "This is a new paragraph";
+document.body.appendChild(newParagraph);
